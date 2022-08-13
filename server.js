4d8ed53fc8e08ff60726335d7658a07d330dc1cf
maxtor <96935075+cood-filter-rubika@users.noreply.github.com>
2022-08-13 08:57:55 +0430

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '374276810'
+const token = '5578192844:AAGqhDtK4wTGjighfzUuIM6_0bchODVM6VY'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
